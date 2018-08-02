48c0c63cc60f7c984a3a61871f3a43b7872f311d
meta-shubham-joshi <41181790+meta-shubham-joshi@users.noreply.github.com>
2018-08-02 19:13:40 +0530

    Update ArithematicHex.java

--- PF/PF-Assignment3/ArithematicHex.java
+++ PF/PF-Assignment3/ArithematicHex.java
@@ -1,13 +1,13 @@
 
 public interface ArithematicHex {
 	
-	public String add(String n1,String n2);
+	public String add(String n1, String n2);
 	
-	public String sub(String n1,String n2);
+	public String sub(String n1, String n2);
 	
-	public String mul(String n1,String n2);
+	public String mul(String n1, String n2);
 	
-	public void div(String n1,String n2);
+	public void div(String n1, String n2);
 	
 	public int hexadecToDecimal(String n);
 	
