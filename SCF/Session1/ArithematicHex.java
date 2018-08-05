b5ba102d86010324314324bb1777ad2423369155
shubhamjoshi <shubhamjoshi@shubham>
2018-08-05 15:52:29 +0530

    Added proper comment before method and removed extra spaces.

---
+++ SCF/Session1/ArithematicHex.java
@@ -0,0 +1,16 @@
+
+public interface ArithematicHex {
+
+	public String add(String n1, String n2);
+
+	public String sub(String n1, String n2);
+
+	public String mul(String n1, String n2);
+
+	public void div(String n1, String n2);
+
+	public int hexadecToDecimal(String n);
+
+	public String decToHexaDec(int n);
+
+}
