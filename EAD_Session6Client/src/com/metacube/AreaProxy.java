079e37caa91b6ade46f5a9dcbabdec67143aedaf
meta-shubham-joshi <41181790+meta-shubham-joshi@users.noreply.github.com>
2018-09-04 19:20:38 +0530

    EAD Session6

---
+++ EAD_Session6Client/src/com/metacube/AreaProxy.java
@@ -0,0 +1,54 @@
+package com.metacube;
+
+public class AreaProxy implements com.metacube.Area {
+  private String _endpoint = null;
+  private com.metacube.Area area = null;
+  
+  public AreaProxy() {
+    _initAreaProxy();
+  }
+  
+  public AreaProxy(String endpoint) {
+    _endpoint = endpoint;
+    _initAreaProxy();
+  }
+  
+  private void _initAreaProxy() {
+    try {
+      area = (new com.metacube.AreaServiceLocator()).getArea();
+      if (area != null) {
+        if (_endpoint != null)
+          ((javax.xml.rpc.Stub)area)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+        else
+          _endpoint = (String)((javax.xml.rpc.Stub)area)._getProperty("javax.xml.rpc.service.endpoint.address");
+      }
+      
+    }
+    catch (javax.xml.rpc.ServiceException serviceException) {}
+  }
+  
+  public String getEndpoint() {
+    return _endpoint;
+  }
+  
+  public void setEndpoint(String endpoint) {
+    _endpoint = endpoint;
+    if (area != null)
+      ((javax.xml.rpc.Stub)area)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+    
+  }
+  
+  public com.metacube.Area getArea() {
+    if (area == null)
+      _initAreaProxy();
+    return area;
+  }
+  
+  public double getTriangleArea(int side1, int side2, int side3) throws java.rmi.RemoteException{
+    if (area == null)
+      _initAreaProxy();
+    return area.getTriangleArea(side1, side2, side3);
+  }
+  
+  
+}
