b63b4005e62c32381101fdef70cfc77fb4c928c5
meta-shubham-joshi <41181790+meta-shubham-joshi@users.noreply.github.com>
2018-09-06 17:29:32 +0530

    Updated code with proper indentation.

--- EAD_Session6/src/com/metacube/Area.java
+++ EAD_Session6/src/com/metacube/Area.java
@@ -1,26 +1,27 @@
 package com.metacube;
 
 /**
-* class containing method to get triagle area
-* created on August 2, 2018
-*/
+ * class containing method to get triagle area 
+ */
 public class Area {
 
-/**
-* method to get area of triangle
-* @param three sides of triangle
-* @return area of triangle calculated
-*/
+	/**
+	 * method to get area of triangle
+	 * 
+	 * @param three sides of triangle
+	 *            
+	 * @return area of triangle calculated
+	 */
 public double getTriangleArea(int side1, int side2, int side3) {
-double area=0;
-try {
-double s=(double)(side1+side2+side3)/2;
-area=(double)Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
-} catch (NumberFormatException ne) {
-ne.printStackTrace();
-}
-return area;
-}
+		
+		double area = 0;
+		try {
+			double s = (double) (side1 + side2 + side3) / 2;
+			area = (double) Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+		} catch (NumberFormatException ne) {
+			ne.printStackTrace();
+		}
+		return area;
+	}
 
 }
-
